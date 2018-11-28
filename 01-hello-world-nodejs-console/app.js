e1c4c1015ffb6e4f3131795c39c81541ce4a04e9
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-11-27 23:30:59 -0400

    adicao do exemplo 01

---
+++ 01-hello-world-nodejs-console/app.js
@@ -0,0 +1,11 @@
+/*
+  Oficina Como Criar Coisas Inteligentes com NodeJS e Arduino
+  Feira Polo Digital de Manaus - 28/11/2018
+  Facilitador:       Prof. Orlewilson Bentes Maia
+  Data Criacao:      27/11/2018
+  Data Atualizacao:  27/11/2018
+  Descricao:         Hello World NodeJS no console
+*/
+
+// mostra mensagem no console
+console.log("Hello World. Seja bem vindo ao NodeJS!");
